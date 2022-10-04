eee24de8839f3f47020c834d2de5e9d8465eb868
jmmunoza <jmmunoza@eafit.edu.co>
2022-10-04 10:01:51 -0500

    Haciendo la lectura de archivos

--- Server/server.c
+++ Server/server.c
@@ -3,39 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
+
 #include "../Util/constants.c"
 
 //#include "../Util/socketList.c"
 
-struct sockaddr_in socketTCPAddress;
-SOCKET socketTCP;
+struct sockaddr_in socketProxyAddress;
+SOCKET socketProxy;
 
 void sendResponse(SOCKET client, char *response){
 	send(client , response , strlen(response) , 0);
 }
 
-char* createResponse(char *petition){
-	char* response = "{'response' : 'Hello Client'}";
-	char  response_len[10];
-	sprintf(response_len, "%d", strlen(response));
-
-	// Building the JSON response
-	char *message = malloc (sizeof (char) * RESPONSE_LEN);
-	strcat(message, "HTTP/1.1 200 OK\r\n");
-	strcat(message, "Content-Type: application/json\r\n");
-	strcat(message, "Content-Length: ");
-	strcat(message, response_len);
-	strcat(message, "\r\n\r\n");
-	strcat(message, response);
-
-	return message;
-}
-
 void receiveRequest(SOCKET client, char *petition){
-	//petition[strlen(petition)-1] = '\0';
-	//char *HTTP_REQUEST = strtok(petition, "\n");
-	//printf(HTTP_REQUEST);
-
 	// --------------------------------------------------------------------
 	// Connect to WEBAPP
 	SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
@@ -45,101 +25,119 @@ void receiveRequest(SOCKET client, char *petition){
     serverAddress.sin_addr.s_addr = inet_addr(SERVER_1_IP);
 
 	if (connect(serverSocket , (struct sockaddr *)&serverAddress , sizeof(serverAddress)) >= 0) {
-        printf("Connected \n");
-		int sendCode;
-
+       
 		// Sending a message
-		if(sendCode = send(serverSocket , petition , strlen(petition) , 0) >= 0) {
-			int recv_size;
+		if(send(serverSocket , petition , strlen(petition) , 0) >= 0) {
 			char server_reply[RESPONSE_LEN]; 
-			
+			int recv_size;
+
 			if ((recv_size = recv(serverSocket , server_reply , RESPONSE_LEN, 0)) != SOCKET_ERROR) {
 				server_reply[recv_size] = '\0';
 				printf(server_reply);
-				
 				sendResponse(client, server_reply);
-
+				
 				if ((recv_size = recv(serverSocket , server_reply , RESPONSE_LEN, 0)) != SOCKET_ERROR) {
 					server_reply[recv_size] = '\0';
-				
-					
 					sendResponse(client, server_reply);
-
-					
 				}
 			}
-			
-			closesocket(client);
+
 			closesocket(serverSocket);
-			
-			printf("-------&&&&&&&&&&&&&&&---");
-			printf("\n");
 		}
     } 
-
-	// --------------------------------------------------------------------
-
-	// Creating the response
-	//char *response = createResponse(HTTP_REQUEST);
-	// Sending the response
-	//sendResponse(client, response);
 }
 
-void initSocket(){
+void initSocketProxy(){
 	// Starting SOCKETS API
 	WSADATA WsaData;
 	WORD wVersionRequerida = MAKEWORD (2, 2);
 	WSAStartup (wVersionRequerida, &WsaData);
 
 	// TCP Socket
-	socketTCP = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	socketProxy = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	// Socket address
-    socketTCPAddress.sin_family      = AF_INET;
-    socketTCPAddress.sin_port        = htons(PORT);
-    socketTCPAddress.sin_addr.s_addr = inet_addr(PROXY_IP);
-} 
-
-void bindSocket(){
-	if( bind(socketTCP ,(struct sockaddr *)&socketTCPAddress , sizeof(socketTCPAddress)) == SOCKET_ERROR) {
-		exit(EXIT_FAILURE);
-	}
-}
-
-void listenSocket(){
-	char client_petition[PETITION_LEN];
-	int recv_size, c;
-	SOCKET new_socket;
-	struct sockaddr_in server , client;
-
-	c = sizeof(struct sockaddr_in);
+    socketProxyAddress.sin_family      = AF_INET;
+    socketProxyAddress.sin_port        = htons(PORT);
+    socketProxyAddress.sin_addr.s_addr = inet_addr(PROXY_IP);
 
 	printf("Listening for new connections in ");
 	printf(PROXY_IP);
 	printf(":");
 	printf("%d", PORT);
 	printf("\n");
+} 
+
+void bindSocketProxy(){
+	if( bind(socketProxy ,(struct sockaddr *)&socketProxyAddress , sizeof(socketProxyAddress)) == SOCKET_ERROR) {
+		exit(EXIT_FAILURE);
+	}
+}
+
+int indexOf(char *string, char e){
+	char *a = strchr(string, e);
+	return (int)(a - string);
+}
+
+void listenSocketProxy(){
+	SOCKET newClient;
+	struct sockaddr_in newClientAddress;
+	int newClientSize = sizeof(struct sockaddr_in);
+
+
+
+
+	
+
+
+
+
+	FILE    *textfile;
+    char    line[1000];
+     
+    textfile = fopen("Server/server_config.txt", "r");
+    while(fgets(line, 1000, textfile)){
+		int init = indexOf(line, '=');
+		int finale = indexOf(line, '\n');
+		
+
+        char subbuff[finale-init];
+		memcpy( subbuff, &line[init + 1], finale );
+		subbuff[finale] = '\0';
+		printf("%d", atoi(subbuff));
+    }
+     
+    fclose(textfile);
+
+
+
 
 	//Listen to incoming connections
-	listen(socketTCP , MAX_CONNECTIONS);
+	listen(socketProxy , MAX_CONNECTIONS);
 
 	while(1) {
 		// Looking for new connections
-		if((new_socket = accept(socketTCP , (struct sockaddr *)&client, &c)) != INVALID_SOCKET ) {
-			if ((recv_size = recv(new_socket , client_petition , PETITION_LEN , 0)) != SOCKET_ERROR){
-				receiveRequest(new_socket, client_petition);
+		if((newClient = accept(socketProxy , (struct sockaddr *)&newClientAddress, &newClientSize)) != INVALID_SOCKET ) {
+
+			// New client has arrived
+			char client_petition[PETITION_LEN];
+			if (recv(newClient , client_petition , PETITION_LEN , 0) != SOCKET_ERROR) {
+
+				// Receiving client's petition
+				receiveRequest(newClient, client_petition);
+				closesocket(newClient);
 			}
 		}
 	}
 
-	closesocket(socketTCP);
+	closesocket(socketProxy);
 	WSACleanup();
 }
 
 int main(int argc, char *argv[]) {
-	initSocket();
-	bindSocket();
-	listenSocket();
+	initSocketProxy();
+	bindSocketProxy();
+	listenSocketProxy();
     
 	return 0;
 }
--- Util/constants.c
+++ Util/constants.c
@@ -1,7 +1,7 @@
 static const int   PORT            = 8080;
 static const int   MAX_CONNECTIONS = 5000;
-static const int   PETITION_LEN    = 10000;
-static const int   RESPONSE_LEN    = 100000;
+static const int   PETITION_LEN    = 100000;
+static const int   RESPONSE_LEN    = 1000000;
 static const char  PROXY_IP[]      = "127.0.0.1";
 
 // Server 1
