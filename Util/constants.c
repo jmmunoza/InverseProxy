667ecaa10dea5459ec64528e61e2b85b63a1ed83
jmmunoza <jmmunoza@eafit.edu.co>
2022-10-30 23:18:27 -0500

    Listo

--- Util/constants.c
+++ Util/constants.c
@@ -1,7 +1,3 @@
 static const int   MAX_CONNECTIONS = 5000;
 static const int   PETITION_LEN    = 100000;
 static const int   RESPONSE_LEN    = 1000000;
-
-// Server 1
-static const char  SERVER_1_IP[]   = "127.0.0.1";
-static const int   SERVER_1_PORT   = 8000;
